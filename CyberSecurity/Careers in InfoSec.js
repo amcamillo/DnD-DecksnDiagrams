8b49021d0485ac3d6ebe77429416303bbc374a63
Camillo <58806811+amcamillo@users.noreply.github.com>
2023-11-17 15:52:32 +1300

    Create Careers in InfoSec.js

---
+++ CyberSecurity/Careers in InfoSec.js
@@ -0,0 +1,38 @@
+//mermaid.js mindmap
+//run it in mermaid.live
+
+mindmap
+    root(Careers in Infosec)
+        Id{{Development}}
+            ::icon(fas fa-code)
+            Id(Pentester)
+            Id(Secure Code dev)
+            Id(Research)
+            Id(Cryptography Engineer)
+        Id{{Design}}
+            ::icon(fas fa-pencil-ruler)
+            Id(Security Architect)
+            Id(Compliance Manager)
+            Id(Consultant – Vendor/Provider)
+            Id(Project Manager)
+        Id{{Operational}}
+            ::icon(fas fa-wrench)
+            Id(Security Engineer)
+            Id(SOC Analyst)
+            Id(Security Systems Adminstrator)
+            id(Support Management)
+            Id(Implementation Engineering)
+            id(Auditor) 
+        Id{{Industry}}
+            ::icon(fas fa-industry)
+            Id(Program Manager – Vendor/Provider)
+            Id(Product Management – Vendor/Provider)
+            Id(Sales Specialist – Vendor/Provider)
+            Id(Customer Success Management – Vendor/Provider)
+        Id{{Management}}
+            ::icon(fas fa-tasks)
+            Id(Security Operations Management)
+            Id(Governance Lead)
+            Id(Assurance Manager)
+            Id(CISO)
+
